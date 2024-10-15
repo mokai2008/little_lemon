58aa088414cb7eb7dc50f12e516511fc6d477744
Michael Gad <mokai2008@gmail.com>
2024-10-15 19:23:41 +0300

    create initial components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header>
+      <img alt="logo" />
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
