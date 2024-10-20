c60eaf18b7e30b2d6b4ca350a3a43a666193dfab
Michael Gad <mokai2008@gmail.com>
2024-10-21 01:06:27 +0300

    added navigate to reserve table button

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -2,8 +2,13 @@ import React from "react";
 import classes from "./CallToAction.module.css";
 import callToAction from "../assets/images/restaurant.jpg";
 import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
 const CallToAction = () => {
+  const navigate = useNavigate();
+  const navigateToReservations = () => {
+    navigate("/booking");
+  };
   return (
     <section className={classes.call}>
       <section className={classes.content}>
@@ -14,7 +19,7 @@ const CallToAction = () => {
             We are a family owned mediterranean restaurant, focused on
             traditional recipes served with a modern twist.
           </p>
-          <Button title="Reserve a table" />
+          <Button title="Reserve a table" onClick={navigateToReservations} />
         </section>
         <section className={classes.item}>
           <img src={callToAction} alt="Little Lemon restaurant" />
