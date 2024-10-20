315bd4ac360304687781f7c6d2c6e13bbd14b18f
Michael Gad <mokai2008@gmail.com>
2024-10-20 17:33:32 +0300

    added the testnomials section with its styles

---
+++ src/components/Testnomials.js
@@ -0,0 +1,52 @@
+import React from "react";
+import classes from "./Testnomials.module.css";
+import item1 from "../assets/images/item1.jpg";
+
+const Testnomials = () => {
+  return (
+    <section className={classes.testo}>
+      <h1>Testimonials</h1>
+      <section className={classes.content}>
+        <section className={classes.item}>
+          <section className={classes.details}>
+            <div>
+              <img src={item1} alt="testo-item1" />
+            </div>
+            <div>8/10</div>
+          </section>
+          <p>Review</p>
+        </section>
+        <section className={classes.item}>
+          <section className={classes.details}>
+            <div>
+              <img src={item1} alt="testo-item1" />
+            </div>
+            <div>8/10</div>
+          </section>
+          <p>Review</p>
+        </section>
+        <section className={classes.item}>
+          <section className={classes.details}>
+            <div>
+              <img src={item1} alt="testo-item1" />
+            </div>
+            <div>8/10</div>
+          </section>
+          <p>Review</p>
+        </section>
+        <section className={classes.item}>
+          <section className={classes.details}>
+            <div>
+              <img src={item1} alt="testo-item1" />
+            </div>
+            <div>8/10</div>
+          </section>
+          <p>Review</p>
+        </section>
+        <section className={classes.item}></section>
+      </section>
+    </section>
+  );
+};
+
+export default Testnomials;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CallToAction from "../components/CallToAction";
 import Specials from "../components/Specials";
+import Testnomials from "../components/Testnomials";
 const Homepage = () => {
   return (
     <>
       <main className="main">
         <CallToAction />
         <Specials />
+        <Testnomials />
       </main>
     </>
   );
